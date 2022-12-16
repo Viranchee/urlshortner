35084f3a3e0a50c7a43ccd55053ebfbcbab2783e
Viranchee Lotia <viranchee@outlook.com>
2022-12-16 01:58:57 -0500

    Passing States and props around FTW

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,23 @@
+import { Button, Stack, Text } from "@mantine/core";
+import { Link } from "@remix-run/react";
+import React, { useState } from "react";
+import { FloatingLabelInput } from "~/components/FloatingLabelInput";
+
 export default function Index() {
+  const [isShortened, setIsShortened] = useState(false);
+  const [value, setValue] = useState("");
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Stack align="center" mt={50}>
+      <Text size="xl" weight={500}>
+        Welcome to my URL shortner
+      </Text>
+      <FloatingLabelInput value={value} setValue={setValue} />
+      <Button onClick={() => setIsShortened(!(value == ""))}>Submit</Button>
+      <Text hidden={!isShortened}>
+        Shortened URL
+        <Link to={value}>{value}</Link>
+      </Text>
+    </Stack>
   );
 }
