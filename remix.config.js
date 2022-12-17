ec3488007852776e0e7186d3590d1eb46d783ffe
Viranchee Lotia <viranchee@outlook.com>
2022-12-17 01:04:52 -0500

    Try without override

--- remix.config.js
+++ remix.config.js
@@ -1,27 +1,29 @@
-const { withEsbuildOverride } = require("remix-esbuild-override");
-const GlobalsPolyfills =
-  require("@esbuild-plugins/node-globals-polyfill").default;
-
-withEsbuildOverride((option, { isServer }) => {
-  if (isServer)
-    option.plugins = [
-      GlobalsPolyfills({
-        buffer: true,
-      }),
-      ...option.plugins,
-    ];
-
-  return option;
-});
+const nodepoly = require('@esbuild-plugins/node-globals-polyfill').default
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "cloudflare-pages",
-  server: "./server.js",
+  serverBuildTarget: 'cloudflare-workers',
+  server: './server.js',
   devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "functions/[[path]].js",
-  // publicPath: "/build/",
-};
+  ignoredRouteFiles: ['.*'],
+
+  esbuildOverride: (option, { isServer, isDev }) => {
+    option.plugins = option.plugins || []
+    option.plugins.push(nodepoly({ buffer: true }))
+
+    option.define = option.define || {}
+    option.define.global = 'globalThis'
+
+    return option
+  }
+}
+// module.exports = {
+//   serverBuildTarget: "cloudflare-pages",
+//   server: "./server.js",
+//   devServerBroadcastDelay: 1000,
+//   ignoredRouteFiles: ["**/.*"],
+//   // appDirectory: "app",
+//   // assetsBuildDirectory: "public/build",
+//   // serverBuildPath: "functions/[[path]].js",
+//   // publicPath: "/build/",
+// };
